f2f74c369b895ab666ee738c60d666734ca247e4
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2021-10-06 18:16:11 +0300

    بناء السلايدر واستخدام owl carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,21 @@ $(function () {
         $(".header .menu-website").removeClass("show");
     })
 });
+
+// Trigger The Slider
+$('.slider .owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
